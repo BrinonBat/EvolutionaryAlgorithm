50bd55caa9addd262a10221c0a5293f38009c0e9
BrinonBat <baptiste@brinon.eu>
2021-10-10 16:25:14 +0200

    fitness functions are ready

--- V1.0.0/fitness.py
+++ V1.0.0/fitness.py
@@ -1,6 +1,24 @@
+from statistics import pstdev,mean
 
+#generate statistics on the population
+#   population: list of bit vectors
+#   return a list as follow [mean,standard deviation,min,max]
 def evaluation(population):
-    pass
+    #evaluate the fitness of each member to calculate statistics on it
+    pop_fit=[]
+    for i in range(len(population)):
+        pop_fit.append(fitness(population[i]))
 
+    print(pop_fit)
+    #calculate statistics
+    fit_mean=mean(pop_fit)
+    std_dev=pstdev(pop_fit,mu=fit_mean)
+
+    return([fit_mean,std_dev,min(pop_fit),max(pop_fit)])
+
+#evaluate x depending on the quantity of "1" it has
+#   x: bit vector to be tested
+#   return a float in [0,1]
 def fitness(x):
-    pass
+    return float(mean(x))
+    
