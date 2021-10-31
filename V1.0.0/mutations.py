24488f85c96a26390e01b7fad3093fb0ba3f63cc
BrinonBat <baptiste@brinon.eu>
2021-10-31 20:14:32 +0100

    corrected some errors, now run correctly

--- V1.0.0/mutations.py
+++ V1.0.0/mutations.py
@@ -3,31 +3,32 @@ import random as rand
 #used for some testing
 def mutation1(population):
     member=population[rand.randint(0,len(population)-1)]
-    pos=rand.randint(0,len(member[0]-1))
+    print(member)
+    pos=rand.randint(0,len(member)-1)
     member[pos]=int(not(member[pos]))
 
     member=population[rand.randint(0,len(population)-1)]
-    pos=rand.randint(0,len(member[0]-1))
+    pos=rand.randint(0,len(member)-1)
     member[pos]=int(not(member[pos]))
 
 #Change the value of one of the bit
 def oneFlip(population):
     member=population[rand.randint(0,len(population)-1)]
-    pos=rand.randint(0,len(member[0]-1))
+    pos=rand.randint(0,len(member)-1)
     member[pos]=int(not(member[pos]))
 
 #Change the value of three bits
 def threeFlip(population):
     member=population[rand.randint(0,len(population)-1)]
     for i in range(3):
-        pos=rand.randint(0,len(member[0]-1))
+        pos=rand.randint(0,len(member)-1)
         member[pos]=int(not(member[pos]))
 
 #Change the value of five bits
 def fiveFlip(population):
     member=population[rand.randint(0,len(population)-1)]
     for i in range(5):
-        pos=rand.randint(0,len(member[0]-1))
+        pos=rand.randint(0,len(member)-1)
         member[pos]=int(not(member[pos]))
 
 #each bit have 1/nbBit chances to be flipped
