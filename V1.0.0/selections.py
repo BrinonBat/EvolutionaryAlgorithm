3ab164721fb3ff94fb3c49771b6926226ee1d9d9
BrinonBat <baptiste@brinon.eu>
2021-10-31 20:14:59 +0100

    added a new selection function

--- V1.0.0/selections.py
+++ V1.0.0/selections.py
@@ -1,6 +1,7 @@
 import fitness
-
+import random
 #parent selection functions
+#testing function
 def parent1(population):
     return population[-4:]
 
@@ -9,3 +10,21 @@ def survivor1(population,offspring):
     for member in offspring:
         fitness.insert(member,population)
         population.pop(0)
+
+#random choice of 1/4th of the population
+#then pop out the worsts of them
+def bestOfAFourth(population,offspring):
+    out=len(offspring)
+    for member in offspring:
+        fitness.insert(member,population)
+    
+    print(population)
+
+    selecteds=random.sample(range(0, int(len(population)-1)),int(len(population)/4))
+    print(selecteds)
+    selecteds=sorted(selecteds)[-out:]
+    print(selecteds)
+    for mem in selecteds:
+        population.pop(mem)
+
+    print(population)
