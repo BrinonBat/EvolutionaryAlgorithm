234a0d00014dc9b5dab81c55541c6e90beaa2ca3
BrinonBat <baptiste@brinon.eu>
2021-11-23 19:09:44 +0100

    added a seed to the mutations random

--- V1.0.0/mutations.py
+++ V1.0.0/mutations.py
@@ -1,7 +1,8 @@
 import random as rand
 
 #used for some testing
-def mutation1(population):
+def mutation1(population,seed):
+    rand.seed(seed)
     member=population[rand.randint(0,len(population)-1)]
     print(member)
     pos=rand.randint(0,len(member)-1)
@@ -12,27 +13,31 @@ def mutation1(population):
     member[pos]=int(not(member[pos]))
 
 #Change the value of one of the bit
-def oneFlip(population):
+def oneFlip(population,seed):
+    rand.seed(seed)
     member=population[rand.randint(0,len(population)-1)]
     pos=rand.randint(0,len(member)-1)
     member[pos]=int(not(member[pos]))
 
 #Change the value of three bits
-def threeFlip(population):
+def threeFlip(population,seed):
+    rand.seed(seed)
     member=population[rand.randint(0,len(population)-1)]
     for i in range(3):
         pos=rand.randint(0,len(member)-1)
         member[pos]=int(not(member[pos]))
 
 #Change the value of five bits
-def fiveFlip(population):
+def fiveFlip(population,seed):
+    rand.seed(seed)
     member=population[rand.randint(0,len(population)-1)]
     for i in range(5):
         pos=rand.randint(0,len(member)-1)
         member[pos]=int(not(member[pos]))
 
 #each bit have 1/nbBit chances to be flipped
-def bitFlip(population):
+def bitFlip(population,seed):
+    rand.seed(seed)
     member=population[rand.randint(0,len(population)-1)]
     for i in member:
         if rand.randint(0,len(member)-1)==i : member[i]==int(not(member[i]))
