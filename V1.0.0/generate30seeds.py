d425e65f205629a75c8e5071f359aacbaf6a1a61
BrinonBat <baptiste@brinon.eu>
2021-12-03 16:53:30 +0100

    script for generating random seeds

---
+++ V1.0.0/generate30seeds.py
@@ -0,0 +1,9 @@
+import random as rand
+import csv
+with open('config.csv','a',newline='') as csvfile:
+    csv_writer = csv.writer(csvfile, delimiter=' ',dialect='unix',quoting=csv.QUOTE_NONE)
+    seeds=[]
+    for i in range(30):
+        seeds.append(rand.randint(0,9999999))
+    csv_writer.writerow(seeds)
+
