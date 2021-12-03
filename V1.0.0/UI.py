4878b163e0f43b8b4dbb6a2a1312217d41736a6e
BrinonBat <baptiste@brinon.eu>
2021-12-03 16:52:20 +0100

    csvfile -> csv_file

--- V1.0.0/UI.py
+++ V1.0.0/UI.py
@@ -7,8 +7,8 @@ def generateCSVName():
 #create the CSV of the current try
 def createCSV(crossover_name,mutation_name,survivor_selection_name,parent_selection_name):
     csv_name=generateCSVName()
-    with open("V1.0.0/results/"+csv_name+".csv", 'a', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=';',dialect='unix',quoting=csv.QUOTE_NONE)
+    with open("V1.0.0/results/"+csv_name+".csv", 'a', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=';',dialect='unix',quoting=csv.QUOTE_NONE)
         csv_writer.writerow(["crossover","mutation","survivor_selection","parent_selection"])
         csv_writer.writerow([crossover_name,mutation_name,survivor_selection_name,parent_selection_name])
         csv_writer.writerow("")
@@ -19,8 +19,8 @@ def createCSV(crossover_name,mutation_name,survivor_selection_name,parent_select
 #CSV format line is {cycle_number;min;mean;max;std_deviation}
 def updateCSV(cycle_number,population,csv_name):
     stats=fitness.evaluation(population)
-    with open("V1.0.0/results/"+csv_name+".csv", 'a', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=';',dialect='unix',quoting=csv.QUOTE_NONE)
+    with open("V1.0.0/results/"+csv_name+".csv", 'a', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=';',dialect='unix',quoting=csv.QUOTE_NONE)
         csv_writer.writerow([cycle_number]+stats)
     return stats[2]
 
@@ -29,16 +29,16 @@ def updateCSV(cycle_number,population,csv_name):
 #CSV's first line: {Version;vector_size;population_size;nb_cycle;crossover;mutation;survivor_selection;parents_selection;min;mean;max;std_deviation}
 def saveResults(vector_size,population_size,nb_cycle,crossover_name,mutation_name,survivor_selection_name,parent_selection_name,final_stats):
     print(final_stats)
-    with open('global.csv', 'a', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=';',dialect='unix',quoting=csv.QUOTE_NONE)
+    with open('global.csv', 'a', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=';',dialect='unix',quoting=csv.QUOTE_NONE)
         csv_writer.writerow(["1.0.0",str(vector_size),str(population_size),str(nb_cycle),crossover_name,mutation_name,survivor_selection_name,parent_selection_name]+final_stats)
         print("sauvegarde du résultat effectuée")
 
 
 def show(csv_name):
     #gather the datas
-    with open("V1.0.0/results/"+csv_name+".csv", 'r+', newline='') as csvfile:
-        reader = list(csv.reader(csvfile,delimiter=";"))
+    with open("V1.0.0/results/"+csv_name+".csv", 'r+', newline='') as csv_file:
+        reader = list(csv.reader(csv_file,delimiter=";"))
         mins=[0]
         cycles=[0]
         maxs=[0]
