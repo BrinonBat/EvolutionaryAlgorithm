5d322706644b3e4911a68b0a30bb09b39b4928be
BrinonBat <baptiste@brinon.eu>
2021-12-23 15:00:40 +0100

    corrected bestOfAFourth and randomInsertions

--- V1.0.0/selections.py
+++ V1.0.0/selections.py
@@ -10,31 +10,40 @@ def highFitnessFirst(population,offspring):
 #random choice of 1/4th of the population
 #then pop out the worsts of them
 def bestOfAFourth(population,offspring):
-    out=len(offspring)
+
+    qty_insert=0
+    #insert the offspring in the population
     for member in offspring:
         fitness.insert(member,population)
+        qty_insert+=1
     
+    #select 1/4th of the population's position and sort it
     selecteds=random.sample(range(0, int(len(population)-1)),int(len(population)/4))
-    selecteds=sorted(selecteds)[-out:]
-    for i in range (out):
-        population.pop(selecteds[i]-i)
+    selecteds=sorted(selecteds)[-qty_insert:]
+
+    #pop members which position is in selected
+    for i in range(qty_insert):
+        population.pop(selecteds[i]-i) # -i is the update of the position, because the one stored in selecteds isn't updated when popped out of population
 
 def randomInsertion(population,offspring):
 
-    out=len(population)
+    qty_insert=0
 
+    #insertion
     for member in offspring:
+        qty_insert+=1
         fitness.insert(member,population)
     
-
-    while len(population<out):
+    #deletion
+    while qty_insert>0:
         population.pop(random.randint(0,len(population)-1))
+        qty_insert-=1
 
 # parent selection ====> SELECTION
 # select parents depending on their fitness
-def bestFirst(qty,population):
-    return population[-qty:]
+def bestFirst(population):
+    return population[-2:]
 
 # select parents randomly
-def randomSelection(qty,population):
-    return random.choice(population,k=qty)
+def randomSelection(population):
+    return random.choices(population,k=2)
