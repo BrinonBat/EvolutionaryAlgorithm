cd185b0c2fad5c67ca5020177ee1374c987a3021
BrinonBat <baptiste@brinon.eu>
2022-02-06 17:22:05 +0100

    added wheel

--- V3.0.0/UI.py
+++ V3.0.0/UI.py
@@ -7,12 +7,12 @@ def generateCSVName():
     return str(datetime.datetime.now())
 
 #create the CSV of the current try
-def createCSV(selection_name,crossover_name,mutation_name,insertion_name,cross_proba,mutate_proba,seeds):
+def createCSV(selection_name,crossover_name,insertion_name,cross_proba,mutate_proba,seeds):
     csv_name=generateCSVName()
-    with open("V1.0.0/results/"+csv_name+".csv", 'a', newline='') as csv_file:
+    with open("V3.0.0/results/fit_"+csv_name+".csv", 'a', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=';',dialect='unix',quoting=csv.QUOTE_NONE)
-        csv_writer.writerow(["selection","crossover","mutation","insertion","crossover_proba","mutation_proba"])
-        csv_writer.writerow([selection_name,crossover_name,mutation_name,insertion_name,cross_proba,mutate_proba])
+        csv_writer.writerow(["selection","crossover","insertion","crossover_proba","mutation_proba"])
+        csv_writer.writerow([selection_name,crossover_name,insertion_name,cross_proba,mutate_proba])
         csv_writer.writerow("")
         csv_writer.writerow(["generation","min","mean","max","stddev"]+seeds)
     return csv_name
@@ -20,7 +20,7 @@ def createCSV(selection_name,crossover_name,mutation_name,insertion_name,cross_p
 #update the local CSV of the current try
 #CSV format line is {cycle_number;min;mean;max;std_deviation}
 def register(nb_iter,iter_step,results,csv_name):
-    with open("V1.0.0/results/"+csv_name+".csv", 'a', newline='') as csv_file:
+    with open("V3.0.0/results/fit_"+csv_name+".csv", 'a', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=';',dialect='unix',quoting=csv.QUOTE_NONE)
         for rep_num in range(0,nb_iter+1,iter_step):
             fitnesses_at_step=(results[:,int(rep_num/iter_step)]).tolist()
@@ -31,7 +31,7 @@ def register(nb_iter,iter_step,results,csv_name):
 
 def show(csv_name):
     #gather the datas
-    with open("V1.0.0/results/"+csv_name+".csv", 'r+', newline='') as csv_file:
+    with open("V3.0.0/results/fit_"+csv_name+".csv", 'r+', newline='') as csv_file:
         reader = list(csv.reader(csv_file,delimiter=";"))
         mins=[0]
         cycles=[0]
@@ -58,4 +58,69 @@ def show(csv_name):
     plt.ylabel("value")
     plt.grid()
     plt.legend()
-    plt.savefig("V1.0.0/results/"+csv_name+".png")
+    plt.savefig("V3.0.0/results/"+csv_name+".png")
+    plt.clf()
+
+def createFunctionTracer(li_mutations,csv_name):
+    with open("V3.0.0/results/func_"+csv_name+".csv", 'a', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter=';',dialect='unix',quoting=csv.QUOTE_NONE)
+        csv_writer.writerow(["list, for each seed, of the wheel's probabilities"])
+        csv_writer.writerow("")
+        line=[]
+        for elem in li_mutations:
+            line.append(elem.__name__)
+        csv_writer.writerow(line)
+
+def generateWheelsGraph(wheels_records,max_gen):
+    name="wheels_record"
+    gen=range(0,max_gen)
+    bitflip_curve=[]
+    noflip_curve=[]
+    oneflip_curve=[]
+    threeflip_curve=[]
+    fiveflip_curve=[]
+    for g in gen: # for each generation (our X axis)
+        bitflip_sum=0
+        noflip_sum=0
+        oneflip_sum=0
+        threeflip_sum=0
+        fiveflip_sum=0
+
+        #save the mean of each mutation function probability
+        for s in range(0,30):
+            bitflip_sum+=wheels_records[s][g][0]
+            noflip_sum+=wheels_records[s][g][1]
+            oneflip_sum+=wheels_records[s][g][2]
+            threeflip_sum+=wheels_records[s][g][3]
+            fiveflip_sum+=wheels_records[s][g][4]
+        bitflip_curve.append(bitflip_sum/30)
+        noflip_curve.append(noflip_sum/30)
+        oneflip_curve.append(oneflip_sum/30)
+        threeflip_curve.append(threeflip_sum/30)
+        fiveflip_curve.append(fiveflip_sum/30)
+
+    bitflip=np.array(bitflip_curve)
+    noflip=np.array(noflip_curve)
+    oneflip=np.array(oneflip_curve)
+    threeflip=np.array(threeflip_curve)
+    fiveflip=np.array(fiveflip_curve)
+    
+    plt.plot(gen,bitflip,label="bitflip")
+    plt.plot(gen,noflip,label="noflip")
+    plt.plot(gen,oneflip,label="oneflip")
+    plt.plot(gen,threeflip,label="threeflip")
+    plt.plot(gen,fiveflip,label="fiveflip")
+    plt.axis([0,gen[-1],0,100])
+    plt.xlabel("generations")
+    plt.xticks(gen[1::500])
+    plt.ylabel("probability")
+    plt.grid()
+    plt.legend()
+    plt.savefig("V3.0.0/results/"+name+".png")
+
+        
+
+
+
+
+
